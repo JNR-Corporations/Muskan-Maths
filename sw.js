5bb2e78f3da97b87ffd57df860b0c039dd6d0a5b
raj-verma-web <rajyvverma@zohomail.in>
2026-07-14 20:05:11 +0530

    Update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "muskans-math-cache-v1";
 const ASSETS_TO_CACHE = [
   "./",
-  "./muskan_maths_premium.html",
+  "./index.html",
   "./manifest.json",
   "https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&family=Quicksand:wght@400;600;700&display=swap",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
